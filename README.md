71707d41d898cffcce7d97cafdbe85270109f2ad
Jim Grant <jimgrant@gmail.com>
2015-04-14 17:20:53 -0700

    remove unicode strings so that repl.it works

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cf-exam-library
+
+**[Demo on repl.it](http://repl.it/ijf)**
+
+A model of a public library, using Library, Shelf, and Book classes in Python.
+
+This was created for the CodeFellows Python Coding Challenge.
--- library.py
+++ library.py
@@ -7,7 +7,7 @@ class Library(object):
         self._shelves = {}
 
     def __str__(self):
-        return u"{}".format(self.name)
+        return "{}".format(self.name)
 
     def add_shelves(self, *args):
         """Add shelf instances to the library. Can also accept
@@ -25,7 +25,7 @@ class Library(object):
             if shelf_key not in self._shelves:
                 self._shelves[shelf_key] = shelf_to_add
             else:
-                print(u"Cannot add '{shelf}': shelf already exists!"
+                print("Cannot add '{shelf}': shelf already exists!"
                       .format(shelf=shelf_key))
 
     def get_shelf(self, shelf_name):
@@ -33,7 +33,7 @@ class Library(object):
         try:
             return self._shelves[shelf_name]
         except KeyError:
-            print(u"Shelf named {shelf} not found in {library}"
+            print("Shelf named {shelf} not found in {library}"
                   .format(shelf=shelf_name, library=self.name))
 
     def report_all_shelves(self):
@@ -43,28 +43,28 @@ class Library(object):
 
     def report_all_books(self):
         """Pretty print all books in the library."""
-        print(u"= All Books at {library} =".format(library=self))
-        print(u"+ {:^30} | {:^30} | {:^10} +"
-              .format(u"Title", u"Author", u"Copies"))
+        print("= All Books at {library} =".format(library=self))
+        print("+ {:^30} | {:^30} | {:^10} +"
+              .format("Title", "Author", "Copies"))
         for shelf in self._shelves.itervalues():
-            print(u"Shelf - {shelf}".format(shelf=shelf))
+            print("Shelf - {shelf}".format(shelf=shelf))
             shelf.report_books()
 
 
 class Shelf(object):
-    def __init__(self, name=u""):
+    def __init__(self, name=""):
         self.name = name
         self._books = {}
 
     def __str__(self):
-        return u"{}".format(self.name)
+        return "{}".format(self.name)
 
     def get_book(self, title):
         """Retrieve a Book instance from the shelf by title."""
         if title in self._books:
             return self._books[title]
         else:
-            print(u"{title} not found in {shelf}!"
+            print("{title} not found in {shelf}!"
                   .format(title=title, shelf=self.name))
 
     def add_books(self, *args):
@@ -89,7 +89,7 @@ class Shelf(object):
         if title in self._books:
             del self._books[title]
         else:
-            print(u"{title} not found in {shelf}!"
+            print("{title} not found in {shelf}!"
                   .format(title=title, shelf=self.name))
 
     def report_books(self):
@@ -107,23 +107,23 @@ class Book(object):
         self._copies = self._maxcopies = copies
 
     def __str__(self):
-        return (u"Title: {title}\n"
-                u"Author: {author}\n"
-                u"Pages: {page_count}\n"
-                u"Copies: {copies}\n"
-                u"Description: {description}"
+        return ("Title: {title}\n"
+                "Author: {author}\n"
+                "Pages: {page_count}\n"
+                "Copies: {copies}\n"
+                "Description: {description}"
                 .format(title=self.title, author=self.author,
                         page_count=self.page_count, copies=self.copy_str(),
                         description=self.desc))
 
     def copy_str(self):
         """Format a string reporting copy information."""
-        return u"{} / {}".format(self._copies, self._maxcopies)
+        return "{} / {}".format(self._copies, self._maxcopies)
 
     def report(self):
         """Format a string for purposes of printing important book info."""
         info = self.title, self.author, self.copy_str()
-        return u"+ {:<30} | {:<30} | {:^10} +".format(*info)
+        return "+ {:<30} | {:<30} | {:^10} +".format(*info)
 
     def enshelf(self, shelf):
         """Add book to a Shelf instance."""
@@ -140,7 +140,7 @@ class Book(object):
 
     def report_copies(self):
         """Report how many copies of this book are available."""
-        print(u"{title} now has {copies} copies."
+        print("{title} now has {copies} copies."
               .format(title=self.title, copies=self.copy_str()))
 
     def check_out(self):
@@ -149,54 +149,55 @@ class Book(object):
             self._copies -= 1
             self.report_copies()
         else:
-            print(u"Cannot check out {title}: no more copies!"
+            print("Cannot check out {title}: no more copies!"
                   .format(title=self.title))
 
     def check_in(self):
         """Check in a copy of the book."""
         if self._copies < self._maxcopies:
             self._copies += 1
+            self.report_copies()
         else:
-            print(u"All copies of {title} already accounted for!"
+            print("All copies of {title} already accounted for!"
                   .format(title=self.title))
 
 
 if __name__ == "__main__":
     # Create a library with a name
-    library = Library(u"Bellevue Public Library")
+    library = Library("Bellevue Public Library")
 
     # Shelves can be created as their own instances...
-    shelf1 = Shelf(u"Fantasy")
-    shelf2 = Shelf(u"Science Fiction")
+    shelf1 = Shelf("Fantasy")
+    shelf2 = Shelf("Science Fiction")
 
     # ... or by passing a string to the library's add method
-    library.add_shelves(shelf1, shelf2, u"Boring Adult Stuff")
+    library.add_shelves(shelf1, shelf2, "Boring Adult Stuff")
 
     # A book can be created with all information up front
-    book1 = Book(title=u"A Game of Thrones",
-                 author=u"George R. R. Martin",
+    book1 = Book(title="A Game of Thrones",
+                 author="George R. R. Martin",
                  page_count=864,
-                 desc=u"Here is the first volume in George R. R. Martin's "
-                      u"cycle of novels that includes A Clash of Kings and A "
-                      u"Storm of Swords.",
+                 desc="Here is the first volume in George R. R. Martin's "
+                      "cycle of novels that includes A Clash of Kings and A "
+                      "Storm of Swords.",
                  copies=2)
 
     # Or just with a title
-    book2 = Book(u"The Lord of the Rings")
+    book2 = Book("The Lord of the Rings")
 
     # Like shelves, a string passed to the add method will also create a book
-    shelf1.add_books(book1, book2, u"The Scar")
+    shelf1.add_books(book1, book2, "The Scar")
 
     # A book can be retrieved at the shelf level
-    book3 = shelf1.get_book(u"The Scar")
+    book3 = shelf1.get_book("The Scar")
 
     # Which allows the book's information to be added / updated later
-    book3.author = u"China Miéville"
+    book3.author = "China Mieville"
     book3.page_count = 608
-    book3.description = (u"In the third book in an astounding, genre-breaking "
-                         u"run, China Mieville expands the horizon beyond the "
-                         u"boundaries of New Crobuzon, setting sail on the "
-                         u"high seas of his ever-growing world of Bas Lag.")
+    book3.description = ("In the third book in an astounding, genre-breaking "
+                         "run, China Mieville expands the horizon beyond the "
+                         "boundaries of New Crobuzon, setting sail on the "
+                         "high seas of his ever-growing world of Bas Lag.")
 
     # Patrons can check out books, which updates the book's copy count
     book1.check_out()
@@ -209,7 +210,7 @@ if __name__ == "__main__":
     book2.check_in()
 
     # Adding a book with the same title to a shelf will add a copy
-    shelf1.add_books(u"The Scar")
+    shelf1.add_books("The Scar")
 
     # Copy information can be seen in the library's book reporting output
     library.report_all_books()
